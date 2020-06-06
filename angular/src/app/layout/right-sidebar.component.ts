4ff47dd6073b0bc1d0e590948e4f06a503a9567a
bilal.rashid <bilal.rashid@slickwhiz.com>
2020-06-07 00:07:09 +0500

    add settings front end

--- angular/src/app/layout/right-sidebar.component.ts
+++ angular/src/app/layout/right-sidebar.component.ts
@@ -40,6 +40,9 @@ export class RightSideBarComponent extends AppComponentBase implements OnInit {
     ];
 
     selectedThemeCssClass = 'red';
+    brandName: string;
+    tagline: string;
+    footer: string;
 
     constructor(
         injector: Injector,
@@ -51,6 +54,9 @@ export class RightSideBarComponent extends AppComponentBase implements OnInit {
     ngOnInit(): void {
         this.selectedThemeCssClass = this.setting.get('App.UiTheme');
         $('body').addClass('theme-' + this.selectedThemeCssClass);
+        this.brandName = this.setting.get('App.Tenant.Brand');
+        this.tagline = this.setting.get('App.Tenant.Tagline');
+        this.footer = this.setting.get('App.Tenant.Footer');
     }
 
     setTheme(theme: UiThemeInfo): void {
@@ -65,6 +71,15 @@ export class RightSideBarComponent extends AppComponentBase implements OnInit {
             $body.addClass('theme-' + theme.cssClass);
 
             this.selectedThemeCssClass = theme.cssClass;
+        });
+    }
+
+    saveSettings() {
+        this._configurationService.changeBrandName(this.brandName).subscribe(() => {
+        });
+        this._configurationService.changeTagLine(this.tagline).subscribe(() => {
+        });
+        this._configurationService.changeFooter(this.footer).subscribe(() => {
         });
     }
 }
