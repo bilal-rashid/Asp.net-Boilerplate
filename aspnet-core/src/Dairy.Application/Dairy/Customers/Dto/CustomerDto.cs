86e765e4f0fc978d02528c87b162329031c3fefa
bilal.rashid <bilal.rashid@slickwhiz.com>
2020-07-11 18:48:53 +0500

    dto changes

--- aspnet-core/src/Dairy.Application/Dairy/Customers/Dto/CustomerDto.cs
+++ aspnet-core/src/Dairy.Application/Dairy/Customers/Dto/CustomerDto.cs
@@ -11,6 +11,7 @@ namespace Dairy.Dairy.Customers.Dto
         public string Description { get; set; }
         public bool IsTemporary { get; set; }
         public string Address { get; set; }
+        public string Area { get; set; }
         public string PrimaryContact { get; set; }
     }
 }
---
+++ aspnet-core/src/Dairy.Core/Dairy/Utilities/OrderType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Dairy.Dairy.Utilities
+{
+    public enum OrderType
+    {
+        Normal = 1,
+        Sample = 2,
+        Waste = 3,
+        Charity = 4
+    }
+}
